27736542fa5dc46b430bd277ef6474840794c3ee
veer260 <57021124+veer260@users.noreply.github.com>
2023-05-12 15:48:01 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+// import { Nunito } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div>
+      <span className="text-2xl font-bold text-rose-500">hello world!</span>
+    </div>
+  );
+}
